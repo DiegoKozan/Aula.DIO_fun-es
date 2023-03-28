556ac7e06935f5f75221d62d6187ec93121f6bdf
Diego Ferreira kozan <113187839+DiegoKozan@users.noreply.github.com>
2023-03-28 16:16:06 -0300

    Add files via upload

---
+++ funcoes2.js
@@ -0,0 +1,16 @@
+function escrevaMeuNome(nome){
+    return 'Meu nome é ' + nome;
+}
+
+
+function verificarIdade(idade){
+    if (idade >= 18){
+        console.log (escrevaMeuNome('Kozan') + ' ok você é maior de idade ');
+    }else {
+        console.log('você não é maior de idade ');
+    }
+}
+verificarIdade(18); 
+
+    
+
